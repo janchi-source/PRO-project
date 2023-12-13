b3e20acb75107f069e41a22372298d3f0d38ff5f
janchi-source <124387739+janchi-source@users.noreply.github.com>
2023-12-13 08:07:46 +0100

    update

---
+++ audio.py
@@ -0,0 +1,31 @@
+class Audio:
+    def __init__(self, song_path):
+        self.song_path = song_path
+        temp_list = song_path.split('/')
+        temp = temp_list[2].split('.')
+        temp_f = temp[0]
+        with open(f"database/{temp_f}.txt", "r") as temp_read:
+            self.author = temp_read.readline().split(';')[1].split('\n')[0]
+            self.album = temp_read.readline().split(';')[1].split('\n')[0]
+            self.duration = temp_read.readline().split(';')[1].split('\n')[0]
+            
+        print(self.author)
+        print(self.album)
+        print(self.duration)
+
+
+def create_audio_objects(path_path):
+    with open(path_path, 'r') as paths_file:
+        file_paths = paths_file.read().splitlines()
+
+    audio_objects = [Audio(path) for path in file_paths]
+    return audio_objects
+
+path_path = "database/paths.txt"
+objects = create_audio_objects(path_path)
+
+
+file_paths = []
+
+audio_objects = [Audio(path) for path in file_paths]
+
